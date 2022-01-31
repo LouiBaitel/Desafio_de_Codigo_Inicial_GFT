7ac71b5f881a1ae19cb47a5951ae04c5b33ef091
Loui Baitel Junges <92049790+LouiBaitel@users.noreply.github.com>
2022-01-31 08:13:05 -0300

    Add files via upload

---
+++ DesafiosIniciais/diferenca/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+ 
+namespace diferenca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int A = int.Parse(Console.ReadLine());
+          int B = int.Parse(Console.ReadLine());
+          int C = int.Parse(Console.ReadLine());
+          int D = int.Parse(Console.ReadLine());
+          int Diferenca;
+         
+          Diferenca = (( A*B ) - ( C*D ));
+        
+          Console.WriteLine($"DIFERENCA = {Diferenca}");
+          Console.ReadKey();
+        }
+    }
+}
